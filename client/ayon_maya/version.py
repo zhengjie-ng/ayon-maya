f4da638f83f6661cd5380fe38e0f7a43299f0dac
Zheng Jie <zhengjie.zjie@gmail.com>
2024-09-24 16:54:09 +0800

    commit

--- client/ayon_maya/version.py
+++ client/ayon_maya/version.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'maya' version."""
+<<<<<<< Updated upstream
 __version__ = "0.2.11"
+=======
+__version__ = "0.2.13-mnm.1"
+>>>>>>> Stashed changes
